617061bdbdedc32356bd943f8fb490f5528adb65
Douglas Henrique <78812406+douglas-henrique-provi@users.noreply.github.com>
2021-08-14 21:53:41 -0300

    Global style added

---
+++ src/pages/_app.tsx
@@ -0,0 +1,22 @@
+// import App from "next/app";
+import type { AppProps /*, AppContext */ } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Next js Boilerplate</title>
+        <link rel="shortcut icon" href="favicon.ico" />
+        <link rel="apple-touch-icon" href="favicon.ico" />
+        <meta name="description" content="Simple project using next js :)" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
