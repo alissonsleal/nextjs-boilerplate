eaa9da93442562f02364eb659d19118d855133c1
Douglas Henrique <douglas.hsp2019@gmail.com>
2021-08-16 20:11:00 -0300

    Plop js added and babel configuration test to locals

---
+++ generators/plopfile.js
@@ -0,0 +1,33 @@
+module.exports = function (plop) {
+  // controller generator
+  plop.setGenerator('component', {
+      description: 'application component logic',
+      prompts: [{
+          type: 'input',
+          name: 'name',
+          message: 'component name please'
+      }],
+      actions: [
+        {
+          type: 'add',
+          path: '../src/components/{{pascalCase name}}/index.tsx',
+          templateFile: 'templates/index.tsx.hbs' 
+        },
+        {
+          type: 'add',
+          path: '../src/components/{{pascalCase name}}/stories.tsx',
+          templateFile: 'templates/stories.tsx.hbs' 
+        },
+        {
+          type: 'add',
+          path: '../src/components/{{pascalCase name}}/style.ts',
+          templateFile: 'templates/style.ts.hbs' 
+        },
+        {
+          type: 'add',
+          path: '../src/components/{{pascalCase name}}/test.tsx',
+          templateFile: 'templates/test.tsx.hbs' 
+        },
+      ]
+  });
+};
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './style'
+
+const Button = () => (
+  <S.Wrapper>
+    <h1>Button</h1>
+  </S.Wrapper>
+)
+
+export default Button
